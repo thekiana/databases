d86d078556852719450900f19acd0ee20b83baa8
thekiana <kianakdonaldson@gmail.com>
2019-11-13 20:03:03 -0800

    Design schema and refactor tests

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,13 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE IF NOT EXISTS messages (
+  `id` INT AUTO_INCREMENT,
+  `username` VARCHAR(10) NOT NULL,
+  `message` VARCHAR(140) NOT NULL,
+  `roomname` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 /* Create other tables and define schemas for them here! */
@@ -14,4 +18,3 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
